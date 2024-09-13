c0c98e861f86787cfb27a391c6e97795f75680b3
Ubuntu <ubuntu@ip-172-31-0-143.ec2.internal>
2024-09-13 15:02:49 +0000

    Actualizacion CORS

--- programa.py
+++ programa.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 import psycopg2  # O mysql.connector para MySQL
 
 app = Flask(__name__)
-CORS(app, resources={r"/api/*": {"origins": "54.165.241.112"}})
+CORS(app, resources={r"/*": {"origins": "54.165.241.112"}})
 
 # Conexión a la base de datos (reemplaza con tus credenciales y la IP de la base de datos en otra instancia)
 conn = psycopg2.connect(
